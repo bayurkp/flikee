a5a3917f4c6477629aab8a9e670903bc9a1ce905
Bayu Rizky Kurnia Pratama <bayurkp.dev@gmail.com>
2025-06-15 23:13:07 +0800

    feat: add stt utils

---
+++ app/utils/stt.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+def _whisper(audio_path: str, output_path: str, model: str = "turbo", output_format: str = "srt", language: str = "id") -> str:
+    command = [
+        "whisper",
+        audio_path,
+        "--model", model,
+        "--output_dir", output_path,
+        "--output_format", output_format,
+        "--language", language,
+        "--word_timestamps", "True",
+        "--max_words_per_line", "7"
+    ]
+
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    if result.returncode != 0:
+        raise Exception(result.stderr)
+
+    return output_path
+
+
+def transcribe_speech(audio_path: str, output_path: str) -> str:
+    """Transcribes speech from an audio file using the OpenAI Whisper API."""
+
+    _whisper(audio_path, output_path)
